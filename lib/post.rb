c4be28cf7a264d90aee80138edcd1786807a36f8
Juvenn Woo <machese@gmail.com>
2009-10-24 16:27:49 +0800

    Model modified for newly vendored sequel
    * reduced indent
    * new creat_table? method

--- lib/post.rb
+++ lib/post.rb
@@ -4,88 +4,85 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/../vendor/syntax'
 require 'syntax/convertors/html'
 
 class Post < Sequel::Model
-	unless table_exists?
-		set_schema do
-			primary_key :id
-			text :title
-			text :body
-			text :tags
-			timestamp :created_at
-		end
-		create_table
-	end
 
-	def url
-		d = created_at
-		"/p/#{id}"
-	end
+  db.create_table? :posts do
+    primary_key :id
+    column :title, :text
+    column :body, :text
+    column :tags, :text
+    column :created_at, :timestamp
+  end
 
-	def full_url
-		Blog.settings[:url_base].gsub(/\/$/, '') + url
-	end
+  def url
+    "/p/#{id}"
+  end
 
-	def body_html
-		to_html(body)
-	end
+  def full_url
+    Blog.settings[:url_base].gsub(/\/$/, '') + url
+  end
 
-	def summary
-		@summary ||= body.match(/(.{200}.*?\n)/m)
-		@summary || body
-	end
+  def body_html
+    to_html(body)
+  end
 
-	def summary_html
-		to_html(summary.to_s)
-	end
+  def summary
+    @summary ||= body.match(/(.{200}.*?\n)/m)
+    @summary || body
+  end
 
-	def more?
-		@more ||= body.match(/.{200}.*?\n(.*)/m)
-		@more
-	end
+  def summary_html
+    to_html(summary.to_s)
+  end
 
-	def linked_tags
-		tags.split.inject([]) do |accum, tag|
-			accum << "<a href=\"/tags/#{tag}\">#{tag}</a>"
-		end.join(" ")
-	end
+  def more?
+    @more ||= body.match(/.{200}.*?\n(.*)/m)
+    @more
+  end
 
-	########
+  def linked_tags
+    tags.split.inject([]) do |accum, tag|
+      accum << "<a href=\"/tags/#{tag}\">#{tag}</a>"
+    end.join(" ")
+  end
 
-	def to_html(markdown)
-		out = []
-		noncode = []
-		code_block = nil
-		markdown.split("\n").each do |line|
-			if !code_block and line.strip.downcase == '<code>'
-				out << Maruku.new(noncode.join("\n")).to_html
-				noncode = []
-				code_block = [""]
-			elsif code_block and line.strip.downcase == '</code>'
-			        code_block << ""
-				convertor = Syntax::Convertors::HTML.for_syntax "ruby"
-				highlighted = convertor.convert(code_block.join("\n"))
-				out << "<code>#{highlighted}</code>"
-				code_block = nil
-			elsif code_block
-				code_block << line
-			else
-				noncode << line
-			end
-		end
-		out << Maruku.new(noncode.join("\n")).to_html
-		out.join("\n")
-	end
+  ########
 
-	def split_content(string)
-		parts = string.gsub(/\r/, '').split("\n\n")
-		show = []
-		hide = []
-		parts.each do |part|
-			if show.join.length < 100
-				show << part
-			else
-				hide << part
-			end
-		end
-		[ to_html(show.join("\n\n")), hide.size > 0 ]
-	end
+  def to_html(markdown)
+    out = []
+    noncode = []
+    code_block = nil
+    markdown.split("\n").each do |line|
+      if !code_block and line.strip.downcase == '<code>'
+	out << Maruku.new(noncode.join("\n")).to_html
+	noncode = []
+	code_block = [""]
+      elsif code_block and line.strip.downcase == '</code>'
+	code_block << ""
+	convertor = Syntax::Convertors::HTML.for_syntax "ruby"
+	highlighted = convertor.convert(code_block.join("\n"))
+	out << "<code>#{highlighted}</code>"
+	code_block = nil
+      elsif code_block
+	code_block << line
+      else
+	noncode << line
+      end
+    end
+    out << Maruku.new(noncode.join("\n")).to_html
+    out.join("\n")
+  end
+
+  def split_content(string)
+    parts = string.gsub(/\r/, '').split("\n\n")
+    show = []
+    hide = []
+    parts.each do |part|
+      if show.join.length < 100
+	show << part
+      else
+	hide << part
+      end
+    end
+    [ to_html(show.join("\n\n")), hide.size > 0 ]
+  end
 end
